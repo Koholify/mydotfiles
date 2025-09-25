88ff93041348e088afb6bfb48f41ed55f68c8fd9
Kyle Price <kounro211@gmail.com>
2025-09-25 15:25:52 -0400

    update install to save old conf

--- nvim3/install.sh
+++ nvim3/install.sh
@@ -1,8 +1,11 @@
 #! /usr/bin/env bash
 
 CONF_HOME="${HOME}/.config/nvim"
+echo removing old directory to $CONF_HOME.old
+mv $CONF_HOME $CONF_HOME.old
 echo installing to $CONF_HOME
 mkdir -p $CONF_HOME
 cp -r ./lua $CONF_HOME/lua
 cp -r ./lsp $CONF_HOME/lsp
 cp ./init.lua $CONF_HOME/init.lua
+cp ./.luarc.json $CONF_HOME/luarc.json
